d6ba866bc499a420fce3ceca5d47c402ef4e436b
Shailesh Medhekar <shaileshmedhekar15@gmail.com>
2023-04-25 16:41:37 +0530

    create via javaScript

---
+++ .github/workflows/src/create-ruleset.js
@@ -0,0 +1,38 @@
+const { Octokit } = require('@octokit/rest');
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+const createRuleset = async () => {
+  try {
+    const { data: repo } = await octokit.repos.get({ owner: 'my-org', repo: 'my-repo' });
+
+    const ruleset = {
+      name: 'My Ruleset',
+      rules: [
+        {
+          name: 'Rule 1',
+          conditions: {
+            event: 'pull_request',
+            base: 'main',
+            status: 'success'
+          },
+          actions: [
+            'merge'
+          ]
+        }
+      ]
+    };
+
+    const response = await octokit.request('POST /repos/:owner/:repo/code-scanning/sarif-rulesets', {
+      owner: 'medhekars',
+      repo: 'Test-Rulesets',
+      data: ruleset
+    });
+
+    console.log(`Ruleset created with ID ${response.data.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+createRuleset();
